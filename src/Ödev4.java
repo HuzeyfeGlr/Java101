e9675b2ff320cad2ea0630158bace1881fd8f1d2
Huzeyfe Guler <huzeyfeglrr@gmail.com>
2022-09-07 02:53:48 +0300

    First Commit

--- src/Ödev4.java
+++ src/Ödev4.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Ödev4 {
+import java.util.Scanner;
+public class Ödev4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double boy , index ;
+        int kilo;
+        System.out.print("Lütfen boyunuzu giriniz: ");
+        boy = input.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz: ");
+        kilo = input.nextInt();
+        index = (kilo / ( boy * boy ));
+        System.out.println("Vücut kitle indexsiniz  " + index +"'dir");
+
+    }
 }
