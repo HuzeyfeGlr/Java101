6524190b9cd452f4e1e2e35e1660f6c75ac1484e
Huzeyfe Guler <huzeyfeglrr@gmail.com>
2022-09-07 03:17:06 +0300

    First Commit

--- src/Ödev5.java
+++ src/Ödev5.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Ödev5 {
+import java.util.Scanner;
+
+public class Ödev5 {
+
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        double ar , el, dom, muz, pat, total;
+        System.out.println("Armut kaç kilo?");
+        ar= input.nextDouble();
+        System.out.println("Elma kaç kilo?");
+        el= input.nextDouble();
+        System.out.println("Domates kaç kilo?");
+        dom= input.nextDouble();
+        System.out.println("Muz kaç kilo?");
+        muz= input.nextDouble();
+        System.out.println("Patlıcan kaç kilo?");
+        pat= input.nextDouble();
+        total = ((ar * 2.14) + (el * 3.67) + (dom * 1.11) + (muz * 0.95) + (pat * 5) );
+        System.out.print("Ödenmesi gereken tutar: " + total);
+    }
 }
