4e01a3d3e6d9ae1c885753d6ab29ce28f634249f
Huzeyfe Guler <huzeyfeglrr@gmail.com>
2022-09-08 17:40:32 +0300

    First Commit

--- src/Ödev6.java
+++ src/Ödev6.java
@@ -2,13 +2,17 @@ import java.util.Scanner;
 public class Ödev6 {
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
+
         double n1, n2 ;
         System.out.print("Lütfen 1. sayıyı giriniz:");
+
         n1 = input.nextDouble();
         System.out.print("Lütfen 2. sayıyı giriniz:");
         n2 = input.nextDouble();
+
         System.out.println("Yapmak istediğiniz işlemi seciniz\n(+,-,/,*)");
-         char islem ;
+
+        char islem ;
         islem = input.next().charAt(0) ;
 
         switch (islem) {
