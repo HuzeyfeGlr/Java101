7c2f4d558afc722a6fb2886ef251e94779be4260
Huzeyfe <huzeyfe.glrr@gmail.com>
2022-09-16 14:58:43 +0300

    First Commit

--- src/Ödev24.java
+++ src/Ödev24.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Ödev24 {
+import java.util.Scanner;
+public class Ödev24 {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+
+        int n1, n2, ebob= 1;
+        System.out.print("Lütfen 1. sayıyı giriniz: ");
+        n1=input.nextInt();
+
+        System.out.print("Lütfen 2. sayıyı giriniz: ");
+        n2= input.nextInt();
+
+        int i = n1;
+        while(i > 1){
+            if ((n1 % i == 0) && (n2 % i == 0)){
+                ebob=i;
+                System.out.println("Sayıların EBOB' u: " + ebob);
+                break;
+            }
+        i--;
+    }
+        System.out.println("Sayıların EKOK'u: "+ ((n1*n2)/ebob));
+
+    }
 }
